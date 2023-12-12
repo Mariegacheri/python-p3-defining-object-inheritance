56ca91c080840a3aaa8be364aef98c491089c993
marie <mariegacheri@gmail.com>
2023-12-12 17:13:51 +0300

    complete

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
+# car.py
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    # The fill_up_tank method is inherited from the Vehicle class
